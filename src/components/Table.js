05f4b166a2327f16011ba4af24d4c36be55ef3cb
divyambadala43 <divyambadala43@gmail.com>
2023-09-24 18:26:30 +0530

    ADD: Pagination Logic

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,6 +8,8 @@ const Table = () => {
   const [sortOption, setSortOption] = useState("");
   const [tableData, setTableData] = useState(data);
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 2;
 
   const handleSortChange = (event) => {
     const newSortOption = event.target.value;
@@ -26,7 +28,6 @@ const Table = () => {
     const searchTerm = event.target.value;
     setSearchTerm(searchTerm);
 
-    // Filter the data based on the search term
     const filteredData = data.filter((info) => {
       const searchString =
         `${info.customerName} ${info.company} ${info.phoneNumber} ${info.email} ${info.country} ${info.status}`.toLowerCase();
@@ -36,16 +37,13 @@ const Table = () => {
     setTableData(filteredData);
   };
 
-  const tableRows = tableData.map((info) => (
-    <tr key={info.id}>
-      <td>{info.customerName}</td>
-      <td>{info.company}</td>
-      <td>{info.phoneNumber}</td>
-      <td>{info.email}</td>
-      <td>{info.country}</td>
-      <td>{info.status}</td>
-    </tr>
-  ));
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = tableData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <div className={styles.tableContainer}>
@@ -56,19 +54,51 @@ const Table = () => {
         />
         <Sort sortOption={sortOption} handleSortChange={handleSortChange} />
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th className={styles.columnHeading}>Customer Name</th>
-            <th className={styles.columnHeading}>Company</th>
-            <th className={styles.columnHeading}>Phone Number</th>
-            <th className={styles.columnHeading}>Email</th>
-            <th className={styles.columnHeading}>Country</th>
-            <th className={styles.columnHeading}>Status</th>
-          </tr>
-        </thead>
-        <tbody>{tableRows}</tbody>
-      </table>
+      <div className={styles.heading}>Active Members</div>
+      <div className={styles.table}>
+        <table>
+          <thead>
+            <tr>
+              <th className={styles.columnHeading}>Customer Name</th>
+              <th className={styles.columnHeading}>Company</th>
+              <th className={styles.columnHeading}>Phone Number</th>
+              <th className={styles.columnHeading}>Email</th>
+              <th className={styles.columnHeading}>Country</th>
+              <th className={styles.columnHeading}>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentItems.map((info) => (
+              <tr key={info.id}>
+                <td style={{ marginLeft: "20px" }}>{info.customerName}</td>
+                <td>{info.company}</td>
+                <td>{info.phoneNumber}</td>
+                <td>{info.email}</td>
+                <td>{info.country}</td>
+                <td>{info.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {tableData.length > itemsPerPage && (
+        <div className={styles.pagination}>
+          {Array(Math.ceil(tableData.length / itemsPerPage))
+            .fill()
+            .map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentPage === index + 1
+                    ? styles.activePageButton
+                    : styles.pageButton
+                }
+                onClick={() => handlePageChange(index + 1)}>
+                {index + 1}
+              </button>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
